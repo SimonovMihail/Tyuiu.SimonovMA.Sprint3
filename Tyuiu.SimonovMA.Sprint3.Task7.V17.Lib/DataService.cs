56d87a6cb10d1afc4b1c21262d24bb6d20cc3673
Tyuiu.SimonovMA@outlook.com <Tyuiu.SimonovMA@outlook.com>
2024-10-28 21:46:10 +0500

    Task7

---
+++ Tyuiu.SimonovMA.Sprint3.Task7.V17.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.SimonovMA.Sprint3.Task7.V17.Lib
+{
+    public class DataService : ISprint3Task7V17
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                y = (Math.Sin(x) / (Convert.ToDouble(x) + 1.7)) - (Math.Cos(x) * (4.0 * Convert.ToDouble(x))) - 6.0;
+                valueArray[count] = Math.Round(y, 2);
+                count++;
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.SimonovMA.Sprint3.Task7.V17/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Tyuiu.SimonovMA.Sprint3.Task7.V17.Lib;
+
+namespace Tyuiu.SimonovMA.Sprint3.Task7.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов к спринту                  *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Симонов Михаил Алексеевич | РППб-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая выводит таблицу значений функции на заданном*");
+            Console.WriteLine("* диапазоне [-5; 5] с шагом 1. Произвести проверку деления на ноль. При   *");
+            Console.WriteLine("* делении на ноль вернуть значение 0. Значение округлить до двух знаков   *");
+            Console.WriteLine("* после запятой.                                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("------------------");
+            Console.WriteLine("- X         f(x) -");
+            Console.WriteLine("------------------");
+
+            for (int i = 0; i <= len-1; i++)
+            {
+                Console.WriteLine("- {0,5:d}   {1, 6:f2} -", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("------------------");
+            Console.ReadKey();
+        }
+    }
+}
